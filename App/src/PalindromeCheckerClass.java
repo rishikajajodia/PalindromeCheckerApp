88d7cbf18279ae1de8b341ef6e4dfd74bef7fff8
unknown <gajulapallepavankumarreddy@gmail.com>
2026-03-10 09:23:57 +0530

    UC2 - Print a Hardcoded Palindrome Result

--- App/src/PalindromeCheckerClass.java
+++ App/src/PalindromeCheckerClass.java
@@ -1,41 +1,22 @@
-/**
- * ==========================================================
- * MAIN CLASS - UseCase1PalindromeCheckerApp
- * ==========================================================
- *
- * Use Case 1: Application Entry & Welcome Message
- *
- * Description:
- * This class represents the entry point of the
- * Palindrome Checker Management System.
- *
- * At this stage, the application:
- * - Starts execution from the main() method
- * - Displays a welcome message
- * - Shows application version
- *
- * No palindrome logic is implemented yet.
- *
- * The goal is to establish a clear startup flow.
- *
- * @author Developer
- * @version 1.0
- */
+class PalindromeChecker {
 
-public class PalindromeCheckerApp {
-
-    /**
-     * Application entry point.
-     *
-     * This is the first method executed by the JVM
-     * when the program starts.
-     *
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
+        // Hardcoded string
+        String word = "madam";
 
+        // Reverse the string
+        String reversed = "";
+
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if(word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
     }
 }
